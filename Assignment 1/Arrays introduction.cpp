2f2849f21ed3e2fdbbcfe69b3eddb5ac56d46cc2
Priyanka <97823620+Priy-nka@users.noreply.github.com>
2022-09-20 12:39:41 +0530

    Create Arrays introduction.cpp

---
+++ Assignment 1/Arrays introduction.cpp
@@ -0,0 +1,25 @@
+
+//https://www.hackerrank.com/challenges/arrays-introduction/problem?isFullScreen=true
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N;
+    cin>>N;
+    int arr[N];
+    for (int i=0;i<N;i++)
+    {
+        cin>>arr[i];
+    }
+    for (int j=N-1;j>=0;j--)
+    {
+        cout<<arr[j]<<" ";
+    }   
+    return 0;
+}
