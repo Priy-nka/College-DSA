2f248591b526f4d5d25ae7a49258a05e2e15406d
Priyanka <97823620+Priy-nka@users.noreply.github.com>
2022-09-20 12:47:21 +0530

    Create pointer.cpp

---
+++ Assignment 1/pointer.cpp
@@ -0,0 +1,23 @@
+
+//https://www.hackerrank.com/challenges/c-tutorial-pointer/problem?isFullScreen=true
+
+#include <stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+void update(int *a,int *b) {
+    int x=*a,y=*b;
+    *a=x+y;
+    *b=abs(x-y);    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
