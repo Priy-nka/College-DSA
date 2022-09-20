ce2a7ad80951141aa5ff5b21fe9b4311ba7243f8
Priyanka <97823620+Priy-nka@users.noreply.github.com>
2022-09-20 12:32:51 +0530

    Create Input and Output.cpp

---
+++ Assignment 1/Input and Output.cpp
@@ -0,0 +1,18 @@
+
+//https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c,sum;
+    cin>>a>>b>>c;
+    sum=a+b+c;
+    cout<<sum;
+    return 0;
+}
