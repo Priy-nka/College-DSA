c97f3b0677b0f600e1b30be492a3e8ddbd492194
Priyanka <97823620+Priy-nka@users.noreply.github.com>
2022-09-20 17:51:16 +0530

    Create 1D Arrays in C.c

---
+++ Assignment 2/1D Arrays in C.c
@@ -0,0 +1,22 @@
+//https://www.hackerrank.com/challenges/1d-arrays-in-c/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,sum=0;
+    scanf("%d",&n);
+    int arr[n];
+    for (int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (int j=0;j<n;j++)
+    {
+        sum+=arr[j];
+    }
+    printf("%d",sum);
+    return 0;
+}
