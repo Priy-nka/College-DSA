3617680e04db83e00a0473f4b0043a4879530705
Priyanka <97823620+Priy-nka@users.noreply.github.com>
2022-09-20 12:35:08 +0530

    Create Basic Data Types.cpp

---
+++ Assignment 1/Basic Data Types.cpp
@@ -0,0 +1,17 @@
+
+//https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    printf("%d\n%ld\n%c\n%f\n%lf",a,b,c,d,e);
+    return 0;
+}
